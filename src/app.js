52238ad88c15e10e3c4f158162a399359df66791
Premranjan1234 <rprem8955@gmail.com>
2023-12-16 00:59:58 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,4 +11,13 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+import userRouter from './routes/user.router.js'
+
+app.use('/api/v1/users',userRouter)
+
+
+
+
+
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler=(requestHandler)=>{
-     (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
      }
 }
@@ -11,6 +11,10 @@ const asyncHandler=(requestHandler)=>(fn)=>async(req,res,next){
     try{
         await fn(req,res,next)
     }catch(error){
+        res.status(err.code||500).json({
+            succsess:false,
+            message:err.message
+        })
         
     }
 }
