01125d3ff3187657bbed64957f8d986945628077
Premranjan1234 <rprem8955@gmail.com>
2024-01-18 12:37:22 +0530

    Add comment,like,playlist,tweet modals

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -25,8 +25,6 @@ cloudinary.config({
     }
   }
 
-  {/*cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-function(error, result) {console.log(result); });*/}
+  
   
   export {uploadOnCloudinary}
